31f27a78db81ad7b4426307f62853dd0738e7cf3
Yann Amaral <Shinigame97@gmail.com>
2023-01-01 23:49:28 -0300

    first commit

---
+++ JS/PDP.js
@@ -0,0 +1,11 @@
+//Arrow Function
+trocaImagem =(img) =>{
+    img.src='/IMG/PDP/fotoBack.png';
+    //
+}
+
+//ocultar pokemon
+function trocaImagem2(){
+    let img = document.getElementById('mulherfronte');
+    img.src = '/IMG/PDP/fotoFront.png';
+}
